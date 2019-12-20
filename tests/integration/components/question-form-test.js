9290201e3ffa116298506db5063f1d15f30105b4
Joshua Ob <joshuaaob@gmail.com>
2019-12-20 21:03:23 +0000

    Generate question form component

--- tests/integration/components/question-form-test.js
+++ tests/integration/components/question-form-test.js
@@ -1,26 +1,26 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import hbs from 'htmlbars-inline-precompile';
+import { module, skip } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | question-form', function(hooks) {
-  setupRenderingTest(hooks);
+    setupRenderingTest(hooks)
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+    skip('it renders', async function(assert) {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{question-form}}`);
+        await render(hbs`{{question-form}}`)
 
-    assert.equal(this.element.textContent.trim(), '');
+        assert.equal(this.element.textContent.trim(), '')
 
-    // Template block usage:
-    await render(hbs`
+        // Template block usage:
+        await render(hbs`
       {{#question-form}}
         template block text
       {{/question-form}}
-    `);
+    `)
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
-  });
-});
+        assert.equal(this.element.textContent.trim(), 'template block text')
+    })
+})
